75ffcfa51c952751000afb0ede0f71ac41ee7b37
David Davidović <david@davidovic.io>
2018-06-08 05:26:25 +0200

    finish

--- generate-reports.bash
+++ generate-reports.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd build
 for t in {1..6}; do
-    ./polynneal 5 0.99997 1000 300000 csv ../reports/$t < ../test/test$t.txt > ../reports/$t.csv
+    ./polynneal 5 0.99999 300 600000 csv ../reports/$t < ../test/test$t.txt > ../reports/$t.csv
 done
 popd
